2a1932cb7f033393bee43edecd63ca265be9b0bf
Erol <erolcanozgen@gmail.com>
2021-03-29 22:53:23 +0300

    Project initialization

---
+++ ErkoSMS/ErkoSMS/Enums/UserType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ErkoSMS.Enums
+{
+    public enum UserType
+    {
+        Administrator = 0,
+        SalesMan = 1,
+        Purchaser = 2,
+        Accountant = 3,
+        WareHouseMan = 4
+    }
+}
---
+++ ErkoSMS/ErkoSMS/Startup.cs
@@ -0,0 +1,65 @@
+﻿using ErkoSMS.Enums;
+using ErkoSMS.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ErkoSMS.Startup))]
+namespace ErkoSMS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createRolesandUsers();
+        }
+
+        // In this method we will create default User roles and Admin user for login    
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+            if (!roleManager.RoleExists(UserType.Administrator.ToString()))
+            {
+                var role = new IdentityRole();
+                role.Name = UserType.Administrator.ToString();
+                roleManager.Create(role);
+            }
+
+            if (!roleManager.RoleExists(UserType.Accountant.ToString()))
+            {
+                var role = new IdentityRole();
+                role.Name = UserType.Accountant.ToString();
+                roleManager.Create(role);
+
+            }
+            if (!roleManager.RoleExists(UserType.Purchaser.ToString()))
+            {
+                var role = new IdentityRole();
+                role.Name = UserType.Purchaser.ToString();
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists(UserType.SalesMan.ToString()))
+            {
+                var role = new IdentityRole();
+                role.Name = UserType.SalesMan.ToString();
+                roleManager.Create(role);
+
+            }
+
+            if (!roleManager.RoleExists(UserType.WareHouseMan.ToString()))
+            {
+                var role = new IdentityRole();
+                role.Name = UserType.WareHouseMan.ToString();
+                roleManager.Create(role);
+
+            }
+
+        }
+
+    }
+}
