6bc29ef301e1cd8d6f83b955bfcac036da911fa4
Erol <erolcanozgen@gmail.com>
2021-04-03 15:29:50 +0300

    Left Menu can be different for diferent user now.

--- ErkoSMS/ErkoSMS/Controllers/HomeController.cs
+++ ErkoSMS/ErkoSMS/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using ErkoSMS.Enums;
+using ErkoSMS.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,25 @@ namespace ErkoSMS.Controllers
 
     public class HomeController : Controller
     {
+
+        LeftMenuViewModel AdministratorNavigations = new LeftMenuViewModel
+        {
+            Navigations = new List<LeftMenuItem>
+            {
+                    new LeftMenuItem("User Administration", "UserAdministration", "Index"),
+
+            }
+        };
+
+        LeftMenuViewModel AnynomousNavigations = new LeftMenuViewModel
+        {
+            Navigations = new List<LeftMenuItem> {
+                                                        new LeftMenuItem("Home", "Home", "Index"),
+                                                        new LeftMenuItem("About", "Home", "About"),
+                                                        new LeftMenuItem("Contact", "Home", "Contact"),
+                                                    }
+        };
+
         public ActionResult Index()
         {
             ViewBag.Role = User.IsInRole(UserType.Administrator.ToString()) ? UserType.Administrator.ToString() : "";
@@ -32,35 +52,35 @@ namespace ErkoSMS.Controllers
 
         public ActionResult LeftMenu(string selectedMenu)
         {
-            ViewBag.Role = GetUserRole();
+            var userNavigations = GetUserNavigation();
             ViewBag.SelectedMenu = selectedMenu;
-            return PartialView("_LeftMenu");
+            return PartialView("_LeftMenu", userNavigations);
         }
 
-        private string GetUserRole()
+        private LeftMenuViewModel GetUserNavigation()
         {
             if (User.IsInRole(UserType.Administrator.ToString()))
             {
-                return UserType.Administrator.ToString();
+                return AdministratorNavigations;
             }
             if (User.IsInRole(UserType.Accountant.ToString()))
             {
-                return UserType.Accountant.ToString();
+                return AnynomousNavigations;
             }
             if (User.IsInRole(UserType.Purchaser.ToString()))
             {
-                return UserType.Purchaser.ToString();
+                return AnynomousNavigations;
             }
             if (User.IsInRole(UserType.SalesMan.ToString()))
             {
-                return UserType.SalesMan.ToString();
+                return AnynomousNavigations;
             }
             if (User.IsInRole(UserType.WareHouseMan.ToString()))
             {
-                return UserType.WareHouseMan.ToString();
+                return AnynomousNavigations;
             }
 
-            return "";
+            return AnynomousNavigations;
 
         }
 
