e84d80eb9e45b891c057972f3c8b5700167b2ada
Erol <erolcanozgen@gmail.com>
2021-04-04 21:56:04 +0300

    Index page was added for User Administration.

--- ErkoSMS/ErkoSMS/Controllers/UserAdministrationController.cs
+++ ErkoSMS/ErkoSMS/Controllers/UserAdministrationController.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using ErkoSMS.Models;
+using ErkoSMS.ViewModels;
+using Microsoft.AspNet.Identity.Owin;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace ErkoSMS.Controllers
 {
@@ -11,7 +15,16 @@ namespace ErkoSMS.Controllers
         // GET: UserAdministration
         public ActionResult Index()
         {
-            return View();
+            var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+            var users = userManager.Users.Select(x => new UserViewModel
+            {
+                UserName = x.UserName,
+                UserType = userManager.GetRolesAsync(x.Id).ConfigureAwait(false).GetAwaiter().GetResult().FirstOrDefault() ?? ""
+            }).ToList();
+            var userAdministrationViewModel = new UserAdministrationViewModel { Users = users };
+
+            return View(userAdministrationViewModel);
         }
     }
 }
---
+++ ErkoSMS/ErkoSMS/ViewModels/UserAdministrationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ErkoSMS.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ErkoSMS.ViewModels
+{
+    public class UserAdministrationViewModel
+    {
+        public List<UserViewModel> Users { get; set; }
+    }
+
+    public class UserViewModel
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string UserType { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+
+    }
+}
