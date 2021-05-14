56d252d049a57040b0bbe30526ca7ac57c4293c5
ibrahim <ibrahimsckn35@gmail.com>
2021-05-14 22:53:33 +0300

    Sales List view is implemented.

--- ErkoSMS/ErkoSMS/OrderFilterParameters.cs
+++ ErkoSMS/ErkoSMS/OrderFilterParameters.cs
@@ -1,14 +1,16 @@
 ﻿using ErkoSMS.DataAccess.Model;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Web;
 
 namespace ErkoSMS
 {
-    /// <summary>
-    /// This class represent of Alarm Module Operator Filter fields.
-    /// </summary>
     public class OrderFilterParameters
     {
         public SalesState State { get; set; }
-        public IEnumerable<string> Customers { get; set; } = new List<string>();
+        public IEnumerable<string> Customers { get; set; }
+        public Currency Currency { get; set; }
+        public string InvoiceNumber { get; set; }
     }
 }
