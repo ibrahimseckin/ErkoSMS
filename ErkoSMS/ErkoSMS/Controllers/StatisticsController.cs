29c6fde1ec03e506995d3a56087f9bf01fa192c9
Erol <erolcanozgen@gmail.com>
2021-05-29 17:20:36 +0300

    SalesManStatistics view is implemented.

--- ErkoSMS/ErkoSMS/Controllers/StatisticsController.cs
+++ ErkoSMS/ErkoSMS/Controllers/StatisticsController.cs
@@ -1,5 +1,6 @@
 ﻿using ErkoSMS.DataAccess;
 using ErkoSMS.ViewModels;
+using Microsoft.AspNet.Identity.Owin;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -60,6 +61,44 @@ namespace ErkoSMS.Controllers
         }
 
 
+        public ActionResult GetSalesManStatistics(DateTime startDate, DateTime endDate)
+        {
+            var salesManStatistics = new List<SalesManStatisticViewModel>();
+
+            var orderDataService = new SalesDataService();
+            var sales = orderDataService.GetSales(startDate, endDate);
+
+            var SalesOfSalesMan = sales.GroupBy(x => x.SalesUserGuid).ToList();
+
+            foreach (var saleOfSaleMan in SalesOfSalesMan)
+            {
+                var userManager = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+                var salesManStatistic = new SalesManStatisticViewModel();
+                salesManStatistic.SaleManName = userManager.Users.FirstOrDefault(x => x.Id == saleOfSaleMan.Key)?.UserName ?? "";
+                var completedOrders = saleOfSaleMan.Where(x => x.SalesState == DataAccess.Model.SalesState.InvoiceDoneAndPacked).ToList();
+                salesManStatistic.TotalCompletedOrderNumber = completedOrders.Count();
+                salesManStatistic.TotalCompletedOrderIncomeTL = completedOrders.Where(x => x.Currency == DataAccess.Model.Currency.Tl).Sum(y => y.TotalPrice);
+                salesManStatistic.TotalCompletedOrderIncomeEuro = completedOrders.Where(x => x.Currency == DataAccess.Model.Currency.Eur).Sum(y => y.TotalPrice);
+                salesManStatistic.TotalCompletedOrderIncomeDollar = completedOrders.Where(x => x.Currency == DataAccess.Model.Currency.Usd).Sum(y => y.TotalPrice);
+
+                var ongoingOrders = saleOfSaleMan.Where(x => x.SalesState != DataAccess.Model.SalesState.InvoiceDoneAndPacked && x.SalesState != DataAccess.Model.SalesState.Rejected).ToList();
+                salesManStatistic.TotalOngoingOrderNumber = ongoingOrders.Count();
+                salesManStatistic.TotalOngoingOrderIncomeTL = ongoingOrders.Where(x => x.Currency == DataAccess.Model.Currency.Tl).Sum(y => y.TotalPrice);
+                salesManStatistic.TotalOngoingOrderIncomeEuro = ongoingOrders.Where(x => x.Currency == DataAccess.Model.Currency.Eur).Sum(y => y.TotalPrice);
+                salesManStatistic.TotalOngoingOrderIncomeDollar = ongoingOrders.Where(x => x.Currency == DataAccess.Model.Currency.Usd).Sum(y => y.TotalPrice);
+
+                salesManStatistics.Add(salesManStatistic);
+            }
+
+
+            return new JsonResult()
+            {
+                Data = salesManStatistics.OrderByDescending(x => (x.TotalCompletedOrderIncomeTL, x.TotalOngoingOrderIncomeTL)).ToList(),
+                ContentType = "application/json",
+                JsonRequestBehavior = JsonRequestBehavior.AllowGet
+            };
+        }
+
         public ActionResult GetProductStatistics(DateTime startDate, DateTime endDate)
         {
             var productStatistics = new List<ProductHitListViewModel>();
@@ -77,7 +116,7 @@ namespace ErkoSMS.Controllers
                 productStatistic.ProductName = productSale.Key;
                 var completedOrders = productSale.Where(x => x.sale.SalesState == DataAccess.Model.SalesState.InvoiceDoneAndPacked).ToList();
                 productStatistic.TotalCompletedOrderNumber = completedOrders.Count();
-                productStatistic.TotalCompletedOrderIncomeTL = completedOrders.Where(x => x.sale.Currency == DataAccess.Model.Currency.Tl).Sum(y => y.saleDetail.Quantity*y.saleDetail.UnitPrice);
+                productStatistic.TotalCompletedOrderIncomeTL = completedOrders.Where(x => x.sale.Currency == DataAccess.Model.Currency.Tl).Sum(y => y.saleDetail.Quantity * y.saleDetail.UnitPrice);
                 productStatistic.TotalCompletedOrderIncomeEuro = completedOrders.Where(x => x.sale.Currency == DataAccess.Model.Currency.Eur).Sum(y => y.saleDetail.Quantity * y.saleDetail.UnitPrice);
                 productStatistic.TotalCompletedOrderIncomeDollar = completedOrders.Where(x => x.sale.Currency == DataAccess.Model.Currency.Usd).Sum(y => y.saleDetail.Quantity * y.saleDetail.UnitPrice);
 
@@ -93,7 +132,7 @@ namespace ErkoSMS.Controllers
 
             return new JsonResult()
             {
-                Data = productStatistics.OrderByDescending(x => (x.TotalCompletedOrderIncomeTL,x.TotalOngoingOrderIncomeTL)).ToList(),
+                Data = productStatistics.OrderByDescending(x => (x.TotalCompletedOrderIncomeTL, x.TotalOngoingOrderIncomeTL)).ToList(),
                 ContentType = "application/json",
                 JsonRequestBehavior = JsonRequestBehavior.AllowGet
             };
