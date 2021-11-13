5f7696400e180da32500862da833770e2c97bf62
Erol <erolcanozgen@gmail.com>
2021-11-13 10:47:38 +0300

    Invoice information has been added to the template.

--- ErkoSMS/ErkoSMS/App_Start/BundleConfig.cs
+++ ErkoSMS/ErkoSMS/App_Start/BundleConfig.cs
@@ -32,6 +32,7 @@ namespace ErkoSMS
                      "~/Scripts/jquery-ui-1.12.1.min.js",
                      "~/Scripts/jspdf.umd.min.js",
                      "~/Scripts/jspdf-autotable.js",
+                     "~/Scripts/toWords.js",
                      "~/Scripts/DataTables/media/js/jquery.dataTables.min.js",
                       "~/Scripts/DataTables/extensions/Buttons/js/dataTables.buttons.min.js",
                       "~/Scripts/DataTables/extensions/Buttons/js/buttons.html5.min.js"));
