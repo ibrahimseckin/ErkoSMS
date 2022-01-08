cf1dd1472420eaad61aeda3f72bb7fa48725ddf6
Erol <erolcanozgen@gmail.com>
2022-01-08 13:05:02 +0300

    Logging is added for unhandled exceptions.

--- ErkoSMS/ErkoSMS/Global.asax.cs
+++ ErkoSMS/ErkoSMS/Global.asax.cs
@@ -5,17 +5,26 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using NLog;
 
 namespace ErkoSMS
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error()
+        {
+            var httpContext = HttpContext.Current;
+            var exception = Server.GetLastError();
+            Logger.Error(exception, $"Application wide unhandled exception. URL: '{httpContext?.Request.Url}'");
         }
     }
 }
